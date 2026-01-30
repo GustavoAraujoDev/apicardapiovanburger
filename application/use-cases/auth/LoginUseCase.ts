2f1cff4cf8dc7a868f39bc4b90c8484c9e593d0b
GustavoAraújoDev <122275138+GustavoAraujoDev@users.noreply.github.com>
2026-01-30 17:03:45 -0300

    Refactor LoginUserUseCase export method
    Removed the export keyword from the LoginUserUseCase class and added module.exports for compatibility.

--- application/use-cases/auth/LoginUseCase.ts
+++ application/use-cases/auth/LoginUseCase.ts
@@ -1,4 +1,4 @@
-export class LoginUserUseCase {
+ class LoginUserUseCase {
   constructor(
     private readonly userRepo: any,
     private readonly passwordHasher: any,
@@ -36,3 +36,5 @@ export class LoginUserUseCase {
     };
   }
 }
+
+module.exports =  LoginUserUseCase;
