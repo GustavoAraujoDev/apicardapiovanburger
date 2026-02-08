946e55242bece4108bf0d9177dd477159a1b83dd
GustavoAraújoDev <122275138+GustavoAraujoDev@users.noreply.github.com>
2026-02-08 17:38:54 -0300

    Enhance product validation schemas for Create and Update
    Updated the Create and Update validation schemas to include required fields for images, colors, and tamanhos, and set a default for priceHistory.

--- application/validators/productValidator.js
+++ application/validators/productValidator.js
@@ -1,27 +1,31 @@
 const joi = require('joi');
 
 const ProductValidator = {
-    Create: joi.object({
-        name: joi.string().required(),
-        description: joi.string().required(),
-        price: joi.number().required(),
-        category: joi.string().required(),
-        images: joi.array().items(joi.string()),
-        stock: joi.number().required(),
-        status: joi.string().required(),
-        priceHistory: joi.array()
-    }),
+  Create: joi.object({
+    name: joi.string().required(),
+    description: joi.string().required(),
+    price: joi.number().required(),
+    images: joi.array().items(joi.string()).required(),
+    category: joi.string().required(),
+    colors: joi.array().items(joi.string()).required(),
+    tamanhos: joi.array().items(joi.string()).required(),
+    stock: joi.number().required(),
+    status: joi.string().required(),
+    priceHistory: joi.array().default([])
+  }),
 
-    update: joi.object({
-        name: joi.string(),
-        description: joi.string(),
-        price: joi.number(),
-        category: joi.string(),
-        images: joi.array().items(joi.string()),
-        stock: joi.number(),
-        status: joi.string(),
-        priceHistory: joi.array()
-    })
+  Update: joi.object({
+    name: joi.string(),
+    description: joi.string(),
+    price: joi.number(),
+    images: joi.array().items(joi.string()),
+    category: joi.string(),
+    colors: joi.array().items(joi.string()),
+    tamanhos: joi.array().items(joi.string()),
+    stock: joi.number(),
+    status: joi.string(),
+    priceHistory: joi.array()
+  })
 };
 
 module.exports = ProductValidator;
