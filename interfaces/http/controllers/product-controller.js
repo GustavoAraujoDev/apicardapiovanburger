e576dfb85dd50923b48d5b8f5ba6f5692d77e24e
GustavoAraújoDev <122275138+GustavoAraujoDev@users.noreply.github.com>
2026-02-10 15:55:05 -0300

    Update product-controller.js

--- interfaces/http/controllers/product-controller.js
+++ interfaces/http/controllers/product-controller.js
@@ -11,6 +11,7 @@ const JwtService = require("../auth/JwtService");
 const BcryptPasswordService = require("../security/BcryptPasswordService");
 const UserBlocked = require('../../../domain/events/UserBlocked');
 const UserLoggedIn = require('../../../domain/events/UserLoggedIn');
+const EventDispatcher = require('../../../infra/audit/EventDispatcher');
 // Events
 const eventDispatcher = new EventDispatcher();
 
